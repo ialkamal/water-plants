0b75b2b9742585f5a6b141e69108c039da912fc5
Ismail AlKamal <ismail@nawatt.com>
2020-12-16 05:10:37 +0300

    Added Login Signup Navbar Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import { Route } from "react-router-dom";
+import LogIn from "./components/LogIn";
+import Navbar from "./components/Navbar";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Water My Plants App</h1>
+      <Navbar />
+      <Route path="/" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
     </div>
   );
 }
