338d8f3b990715b470ce6a1652b7ab05b788cd4a
Eduardo Lopez <ed98lopez@gmail.com>
2021-01-04 17:42:59 -0800

    added styling to form, inputs, submit button, and background. Added the post request. Not sure if its to the right endpoint. I keep getting a 404 error message.

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,66 @@
 import React, { useState, useEffect } from "react";
 import * as Yup from 'yup'
-import 'yup-phone'
 import axios from 'axios'
+import styled, {createGlobalStyle, css} from 'styled-components'
+
+
+const GlobalStyle = createGlobalStyle`
+html{height:100%}
+body{
+  font-family: 'Poppins', sans-serif;
+  color:white;
+  background: linear-gradient(to bottom, #43C6AC ,#fffacf)
+}
+
+`
+
+const sharedStyles = css`
+backgroundcolor: #eee;
+height:40px;
+border-radius: 5px;
+border: 2px solid #45B649;
+margin: 10px 0 20px 0;
+padding:20px;
+box-sizing:border-box;`
+
+const FormWrapper = styled.div`
+display:flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+height: 80vh;
+padding:0 20px;
+`
+const StyledForm= styled.form`
+width:100%;
+max-width:700px;
+padding:2.5rem;
+border-radius: 20px;
+background-color: rgba(250, 255, 209, .97);
+box-sizing:border-box;
+box-shadow: 0px 0px 20px rgba(138, 138, 138, .5);
+
+`
+
+const StyledInput = styled.input`
+display:block;
+width 100%;
+${sharedStyles}`
+
+const StyledButton= styled.button`
+padding:1rem;
+background-color:#45B649;
+border-radius:100px;
+width:50%;
+border:none;
+color: white;
+font-weight: 800;;
+
+&:disabled{
+  opacity: .3;
+
+}
+`
 
 const SignUp = () => {
   const initialCredentials = {
@@ -41,31 +100,40 @@ const SignUp = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //handle sending credentials to server and getting the token
+    axios
+      .post("https://water-my-plants-lambda.herokuapp.com/api/register", credentials)
+      .then(res => {console.log("success", credentials, res);
+      })
+      .catch(err => console.log(err))
     setCredentials(initialCredentials);
   };
 
   return (
-    <div>
+    
+<div>
+  <GlobalStyle/>
+  <FormWrapper>
       <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit}>
-        <input
+      <StyledForm onSubmit={handleSubmit}>
+
+      
+        <StyledInput
           type="text"
           name="username"
           autoComplete="username"
           value={credentials.username}
-          placeholder="username"
+          placeholder="email"
           onChange={handleChange}
         />
-        <input
+        <StyledInput
           type="tel"
           name="phoneNumber"
           autoComplete="phone-number"
           value={credentials.phoneNumber}
-          placeholder="phone number"
+          placeholder="phone ex: 123-456-7890"
           onChange={handleChange}
         />
-        <input
+        <StyledInput
           type="password"
           name="password"
           autoComplete="new-password"
@@ -73,9 +141,11 @@ const SignUp = () => {
           placeholder="password"
           onChange={handleChange}
         />
-        <button type="submit" disabled={disabled}>Sign Up</button>
-      </form>
-    </div>
+        <StyledButton type="submit" disabled={disabled}>Sign Up</StyledButton>
+      </StyledForm>
+    </FormWrapper>
+</div>
+    
   );
 };
 
